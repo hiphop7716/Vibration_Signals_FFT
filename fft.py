fb9523f6a085b8d049bab33c0ae16aa7d31948b3
PangTang <hiphop7716@gmail.com>
2019-06-19 14:55:48 +0800

    Clarify between sample rate and total samples, output of this version looks better.

--- fft.py
+++ fft.py
@@ -39,10 +39,6 @@ plt.title('原始資料 (acceleration)', fontproperties=cht_font)
 plt.xlabel('Time (s)')
 plt.ylabel('Amplitude (g-value)')
 
-# plt.legend()
-
-# plt.show()
-
 for times in range(1): #1:x_axis, 2:x&y_axis
     # for i in range(len(files)):
     for i in range(1): #file_amount
@@ -56,45 +52,31 @@ for times in range(1): #1:x_axis, 2:x&y_axis
         N = 100000
         T = 1.0 / 5000.0
 
-        # print(x_axis_g_List)
-        yf_x = fft(x_axis_g_List)
+        yf_x = fft(x_axis_g_List) #include real and image number, need to do abs processing next.
         # Just first half of the spectrum, as the second is the negative copy
-        # xf_x = np.linspace(0.0, 1.0 / (2.0 * T), N)  #X軸 start = 0, end = 2500, axis = 5000(samples)
         xf_x = np.linspace(0.0, 1.0 / (2.0 * T), N/2)  #X軸 start = 0, end = 2500, axis = 5000(samples)
 
-        yf_x_new = 2.0 / N * np.abs(yf_x[:N // 2]) #先marked
+        yf_x_abs = abs(fft(x_axis_g_List)) / ((len(x_axis_g_List) / 2))
+        yf_x_half = yf_x_abs[range(int(len(x_axis_g_List) / 2))]  # 由於對稱性，只取一半區間
 
-        yf1 = abs(fft(x_axis_g_List)) / ((len(x_axis_g_List) / 2))
-        yf2 = yf1[range(int(len(x_axis_g_List) / 2))]  # 由於對稱性，只取一半區間
-
-        print(len(yf2))
-        # print(yf2[:500])
+        # print(len(yf2)) #for check output  after doing FFT
+        # print(yf2[:500]) #for check previous 500 output after doing FFT
 
         yf_x_float6 = []
-        # for index in range(len(yf_x_new)):
-        #     yf_x_float6.append(round(yf_x_new[index], 6))
-        for index in range(len(yf2)):
-            yf_x_float6.append(round(yf2[index], 6))
+        for index in range(len(yf_x_half)):
+            yf_x_float6.append(round(yf_x_half[index], 6))
 
         xf_x_float6 = []
         for index in range(len(xf_x)):
             xf_x_float6.append(round(xf_x[index], 6))
 
-        # print(type(yf_x_float6), yf_x_float6)
-        # print(type(xf_x_float6), xf_x_float6)
-
         plt.subplot(312)
         plt.plot(xf_x_float6, yf_x_float6, label=files[i] + ("_X axis" if times + 1 == 1 else "_Y axis"), color='green')
-        # plt.plot(xf_x, 2.0 / N * np.abs(yf_x[:N // 2]), label=result[i] + ("_X axis" if times + 1 == 1 else "_Y axis"))?
-        # plt.plot(xf_y, 2.0/N * np.abs(yf_y[:N//2]), label=result[i] + "_Y")
         plt.title('頻譜 (FFT Spectrum)', fontproperties=cht_font)
         plt.xlabel("Frenquency (Hz)")
         plt.ylabel("Amplitude (g-value)")
         plt.ylim(-0.1, 0.5)
 
-        # print(xf_x[:10])
-        # print(2.0 / N * np.abs(yf_x[:N // 2]))
-        # np.savetxt('vibration_data\\001', test)
         #output the FFT result
         with open("X_axis_FFT" if times + 1 == 1 else "Y_axis_FFT", 'w') as f:
             for idx in range(2500):
